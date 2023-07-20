0353c8c6a46d6f4da0d35fc8a51b883b4ee8ab1a
peeweeid <65776660+peeweeid@users.noreply.github.com>
2023-07-20 14:52:20 +0700

    Update Peta.js

--- components/Peta.js
+++ components/Peta.js
@@ -12,7 +12,7 @@ function Peta() {
     
     <div className='container  w-2/6 lg:w-4/6 flex justify-center px-4 py-4 lg:py-12 mb-10'>
        <div className="px-4 py-4 bg-third rounded-xl"><div className="gmap_canvas">
-              <iframe className='w-[250px] mx-auto lg:w-[500px] lg:h-[500px] h-[600px] rounded-xl' id="gmap_canvas" src='https://maps.google.com/maps?q=yogyakarta , jl cantel no 15&t=&z=20&ie=UTF8&iwloc=&output=embed'  ></iframe></div>
+              <iframe className='w-[250px] mx-auto lg:w-[500px] lg:h-[500px] h-[600px] rounded-xl' id="gmap_canvas" src='https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d349.99937224195986!2d110.42583877843842!3d-7.033848889887668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwMDInMDIuMSJTIDExMMKwMjUnMzMuNCJF!5e0!3m2!1sid!2sid!4v1689839188433!5m2!1sid!2sid'  ></iframe></div>
               
         </div>
     </div>
